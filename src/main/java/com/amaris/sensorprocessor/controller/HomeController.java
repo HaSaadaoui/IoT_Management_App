d27d33bbe8966c5e6de5a6fe44b0e44244fa142e
VERBEKE Séraphin <seraphin.verbeke@amaris.com>
2025-10-02 15:13:23 +0200

    develop - corrections & dev SV

--- src/main/java/com/amaris/sensorprocessor/controller/HomeController.java
+++ src/main/java/com/amaris/sensorprocessor/controller/HomeController.java
@@ -35,6 +35,8 @@ public class HomeController {
      */
     @GetMapping("/home")
     public String home(Model model) {
+        String loggedUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+        model.addAttribute("loggedUsername", loggedUsername);
         return "home";
     }
 
