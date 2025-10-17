4465c321ca71c2f594a8025f999ec42d4c0022cf
Nakiboudine MOHAMED <naki.md@gmail.com>
2025-10-17 12:39:12 +0200

    feat: connection mysql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
-PRAGMA foreign_keys = ON;
-
 -- Création de la table Gateways
 CREATE TABLE Gateways (
     gateway_id VARCHAR(50) PRIMARY KEY NOT NULL,
@@ -8,11 +6,11 @@ CREATE TABLE Gateways (
     frequency_plan VARCHAR(50) NOT NULL,
     created_at DATETIME NOT NULL,
     building_name VARCHAR(50) NOT NULL,
-    floor_number INTEGER NOT NULL,
+    floor_number INT NOT NULL,
     location_description VARCHAR(50) NULL,
-    antenna_latitude REAL NULL,
-    antenna_longitude REAL NULL,
-    antenna_altitude REAL NULL
+    antenna_latitude FLOAT NULL,
+    antenna_longitude FLOAT NULL,
+    antenna_altitude FLOAT NULL
 );
 
 -- Insertion des valeurs dans la table Gateways
@@ -32,59 +30,29 @@ INSERT INTO Gateways (gateway_id, gateway_eui, ip_address, frequency_plan, creat
 
 -- Création de la table Sensors
 CREATE TABLE Sensors (
-    id_sensor         VARCHAR(50) PRIMARY KEY NOT NULL,
-    device_type       VARCHAR(50) NOT NULL,
-    commissioning_date DATE      NOT NULL,
-    status            BOOLEAN    NOT NULL,
-    building_name     VARCHAR(100) NOT NULL,
-    floor             INTEGER    NOT NULL,
-    location          VARCHAR(50) NULL,
-    id_gateway        VARCHAR(50) NULL,
-    dev_eui           TEXT       NULL,
-    join_eui          TEXT       NULL,
-    app_key           TEXT       NULL,
-    frequency_plan    VARCHAR(50) NULL,
+    id_sensor VARCHAR(50) PRIMARY KEY NOT NULL,
+    device_type VARCHAR(50) NOT NULL,
+    commissioning_date DATE NOT NULL,
+    status BOOLEAN NOT NULL,
+    building_name VARCHAR(100) NOT NULL,
+    floor INT NOT NULL,
+    location VARCHAR(50) NULL,
+    id_gateway VARCHAR(50) NULL,
     FOREIGN KEY (id_gateway) REFERENCES Gateways(gateway_id) ON DELETE SET NULL
 );
 
-
 -- Insertion des valeurs dans la table Sensors
--- (Optionnel) Vider avant de ré-insérer
--- DELETE FROM Sensors;
-
-INSERT INTO Sensors (
-  id_sensor, device_type, commissioning_date, status, building_name, floor, location,
-  id_gateway, dev_eui, join_eui, app_key, frequency_plan
-) VALUES
-('dev_eui_001','device_001','2023-01-01',1,'Batiment A',1,'Bureau 101','gateway-001',
- 'A1B2C3D4E5F60701','0102030405060701','00112233445566778899AABBCCDDEEF0','EU_863_870_TTN'),
-
-('dev_eui_002','device_002','2023-02-01',1,'Batiment B',2,'Bureau 202','gateway-002',
- 'A1B2C3D4E5F60702','0102030405060702','00112233445566778899AABBCCDDEEF1','EU_863_870_TTN'),
-
-('dev_eui_003','device_003','2023-03-01',0,'Batiment A',3,'Bureau 303','gateway-001',
- 'A1B2C3D4E5F60703','0102030405060703','00112233445566778899AABBCCDDEEF2','EU_863_870_TTN'),
-
-('dev_eui_004','device_004','2023-04-01',1,'Batiment C',1,'Bureau 104','gateway-003',
- 'A1B2C3D4E5F60704','0102030405060704','00112233445566778899AABBCCDDEEF3','EU_863_870_TTN'),
-
-('dev_eui_005','device_005','2023-05-01',1,'Batiment D',2,'Bureau 205','gateway-004',
- 'A1B2C3D4E5F60705','0102030405060705','00112233445566778899AABBCCDDEEF4','US_902_928_FSB_2'),
-
-('dev_eui_006','device_001','2023-06-01',0,'Batiment E',3,'Bureau 306','gateway-005',
- 'A1B2C3D4E5F60706','0102030405060706','00112233445566778899AABBCCDDEEF5','AS_920_923'),
-
-('dev_eui_007','device_002','2023-07-01',1,'Batiment A',1,'Bureau 107','gateway-001',
- 'A1B2C3D4E5F60707','0102030405060707','00112233445566778899AABBCCDDEEF6','EU_863_870_TTN'),
-
-('dev_eui_008','device_003','2023-08-01',1,'Batiment B',2,'Bureau 208','gateway-002',
- 'A1B2C3D4E5F60708','0102030405060708','00112233445566778899AABBCCDDEEF7','EU_863_870_TTN'),
-
-('dev_eui_009','device_004','2023-09-01',0,'Batiment C',3,'Bureau 309','gateway-003',
- 'A1B2C3D4E5F60709','0102030405060709','00112233445566778899AABBCCDDEEF8','EU_863_870_TTN'),
-
-('dev_eui_010','device_005','2023-10-01',1,'Batiment D',1,'Bureau 110','gateway-004',
- 'A1B2C3D4E5F6070A','010203040506070A','00112233445566778899AABBCCDDEEF9','US_902_928_FSB_2');
+INSERT INTO Sensors (id_sensor, device_type, commissioning_date, status, building_name, floor, location, id_gateway) VALUES
+('dev_eui_001', 'device_001', '2023-01-01', TRUE, 'Batiment A', 1, 'Bureau 101', 'gateway-001'),
+('dev_eui_002', 'device_002', '2023-02-01', TRUE, 'Batiment B', 2, 'Bureau 202', 'gateway-002'),
+('dev_eui_003', 'device_003', '2023-03-01', FALSE, 'Batiment A', 3, 'Bureau 303', 'gateway-001'),
+('dev_eui_004', 'device_004', '2023-04-01', TRUE, 'Batiment C', 1, 'Bureau 104', 'gateway-003'),
+('dev_eui_005', 'device_005', '2023-05-01', TRUE, 'Batiment D', 2, 'Bureau 205', 'gateway-004'),
+('dev_eui_006', 'device_001', '2023-06-01', FALSE, 'Batiment E', 3, 'Bureau 306', 'gateway-005'),
+('dev_eui_007', 'device_002', '2023-07-01', TRUE, 'Batiment A', 1, 'Bureau 107', 'gateway-001'),
+('dev_eui_008', 'device_003', '2023-08-01', TRUE, 'Batiment B', 2, 'Bureau 208', 'gateway-002'),
+('dev_eui_009', 'device_004', '2023-09-01', FALSE, 'Batiment C', 3, 'Bureau 309', 'gateway-003'),
+('dev_eui_010', 'device_005', '2023-10-01', TRUE, 'Batiment D', 1, 'Bureau 110', 'gateway-004');
 
 -- Création de la table Data_emsdesk
 CREATE TABLE Data_emsdesk (
@@ -114,8 +82,8 @@ INSERT INTO Data_emsdesk (id_sensor, timestamp, humidity, occupancy, temperature
 CREATE TABLE Data_pirlight (
     id_sensor VARCHAR(50),
     timestamp DATE,
-    light_statut INTEGER,
-    pir_statut INTEGER,
+    light_statut INT,
+    pir_statut INT,
     PRIMARY KEY (id_sensor, timestamp),
     FOREIGN KEY (id_sensor) REFERENCES Sensors(id_sensor) ON DELETE CASCADE
 );
@@ -134,23 +102,23 @@ INSERT INTO Data_pirlight (id_sensor, timestamp, light_statut, pir_statut) VALUE
 ('dev_eui_010', '2023-10-01', 1, 0);
 
 -- Création de la table Signal
-CREATE TABLE Signal (
+CREATE TABLE `Signal` (
     id_sensor VARCHAR(50),
     timestamp DATE,
-    value_battery REAL,
-    rssi INTEGER,
-    fport INTEGER,
-    fcntup INTEGER,
-    snr REAL,
-    fcntdown INTEGER,
-    sf INTEGER,
-    frequency_offset REAL,
+    value_battery FLOAT,
+    rssi INT,
+    fport INT,
+    fcntup INT,
+    snr FLOAT,
+    fcntdown INT,
+    sf INT,
+    frequency_offset FLOAT,
     PRIMARY KEY (id_sensor, timestamp),
     FOREIGN KEY (id_sensor) REFERENCES Sensors(id_sensor) ON DELETE CASCADE
 );
 
 -- Insertion des valeurs dans la table Signal
-INSERT INTO Signal (id_sensor, timestamp, value_battery, rssi, fport, fcntup, snr, fcntdown, sf, frequency_offset) VALUES
+INSERT INTO `Signal` (id_sensor, timestamp, value_battery, rssi, fport, fcntup, snr, fcntdown, sf, frequency_offset) VALUES
 ('dev_eui_001', '2023-01-01', 3.7, -90, 1, 10, 12.5, 5, 7, 0.5),
 ('dev_eui_002', '2023-02-01', 3.8, -85, 1, 11, 13.0, 4, 7, 0.6),
 ('dev_eui_003', '2023-03-01', 3.6, -80, 1, 12, 14.0, 6, 7, 0.4),
@@ -167,21 +135,37 @@ CREATE TABLE Users (
     username VARCHAR(50) PRIMARY KEY NOT NULL,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     role VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    icon VARCHAR(100) DEFAULT 'default-avatar.png'
+    icon VARCHAR(100)
 );
 
 -- Insertion des valeurs dans la table Users
-INSERT INTO Users (username, firstname, lastname, password, role, email, icon) VALUES
-('user1', 'John', 'Doe', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'ADMIN', 'john.doe@example.com', 'default-avatar.png'),
-('user2', 'Jane', 'Smith', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'jane.smith@example.com', 'default-avatar.png'),
-('user3', 'Alice', 'Johnson', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'SUPERUSER', 'alice.johnson@example.com', 'default-avatar.png'),
-('user4', 'Bob', 'Brown', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'bob.brown@example.com', 'default-avatar.png'),
-('user5', 'Charlie', 'Davis', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'ADMIN', 'charlie.davis@example.com', 'default-avatar.png'),
-('user6', 'David', 'Wilson', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'david.wilson@example.com', 'default-avatar.png'),
-('user7', 'Eva', 'Martinez', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'SUPERUSER', 'eva.martinez@example.com', 'default-avatar.png'),
-('user8', 'Frank', 'Taylor', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'frank.taylor@example.com', 'default-avatar.png'),
-('user9', 'Grace', 'Anderson', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'SUPERUSER', 'grace.anderson@example.com', 'default-avatar.png'),
-('user10', 'Hannah', 'Thomas', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'ADMIN', 'hannah.thomas@example.com', 'default-avatar.png');
+INSERT INTO Users (username, firstname, lastname, password, role, email) VALUES
+('user1', 'John', 'Doe', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'ADMIN', 'john.doe@example.com'),
+('user2', 'Jane', 'Smith', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'jane.smith@example.com'),
+('user3', 'Alice', 'Johnson', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'SUPERUSER', 'alice.johnson@example.com'),
+('user4', 'Bob', 'Brown', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'bob.brown@example.com'),
+('user5', 'Charlie', 'Davis', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'ADMIN', 'charlie.davis@example.com'),
+('user6', 'David', 'Wilson', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'david.wilson@example.com'),
+('user7', 'Eva', 'Martinez', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'SUPERUSER', 'eva.martinez@example.com'),
+('user8', 'Frank', 'Taylor', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'USER', 'frank.taylor@example.com'),
+('user9', 'Grace', 'Anderson', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'SUPERUSER', 'grace.anderson@example.com'),
+('user10', 'Hannah', 'Thomas', '$2a$10$WcXpO7sR8lJAjp2Nti6jR.Q52y3rNN2UKDTquMAhZWaH1.1qNhmfG', 'ADMIN', 'hannah.thomas@example.com');
+
+
+-- Création de la table pending_users
+CREATE TABLE pending_users (
+    email VARCHAR(100) PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL,
+    firstname TEXT,
+    lastname TEXT,
+    password TEXT NOT NULL,
+    role TEXT,
+    icon TEXT,
+    token_hash TEXT NOT NULL,
+    expires_at TEXT NOT NULL,
+    resend_count INTEGER DEFAULT 0,
+    last_resend_at TEXT
+);
